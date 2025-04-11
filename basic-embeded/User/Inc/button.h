34a4c46cdded0ad7c566cc43fd0f2a8f51212745
이주호 <83685855+vermilion9312@users.noreply.github.com>
2025-04-11 18:19:10 +0900

    .

---
+++ basic-embeded/User/Inc/button.h
@@ -0,0 +1,16 @@
+/*
+ * button.h
+ *
+ *  Created on: Apr 11, 2025
+ *      Author: vermilion9312
+ */
+
+#ifndef INC_BUTTON_H_
+#define INC_BUTTON_H_
+
+#include <stdbool.h>
+#include "main.h"
+
+bool is_button_pressed(void);
+
+#endif /* INC_BUTTON_H_ */
---
+++ basic-embeded/User/Inc/led.h
@@ -0,0 +1,16 @@
+/*
+ * led.h
+ *
+ *  Created on: Apr 11, 2025
+ *      Author: vermilion9312
+ */
+
+#ifndef INC_LED_H_
+#define INC_LED_H_
+
+#include "main.h"
+
+void turn_on_led(void);
+void turn_off_led(void);
+
+#endif /* INC_LED_H_ */
